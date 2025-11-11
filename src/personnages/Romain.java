687685ef896e2ae9520ba5b20cb21bef80876885
siadamine2005-alt <amine.siad@ensia.edu.dz>
2025-11-11 01:49:33 +0100

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,10 +1,12 @@
 package personnages;
 
-import java.time.chrono.MinguoChronology;
+import objets.Equipement;
 
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -44,9 +46,38 @@ public class Romain {
 		assert isInvariantVerified();
 	}
 
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (equipements[0].equals(equipement)) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement);
+			} else {
+				equiperRomain(equipement);
+			}
+			break;
+		case 0:
+			equiperRomain(equipement);
+		}
+
+	}
+
+	private void equiperRomain(Equipement equipement) {
+		System.out.println("Le soldat " + nom + "  s’équipe avec un " + equipement);
+		equipements[nbEquipement] = equipement;
+		nbEquipement++;
+	}
+
 	public static void main(String args[]) {
 
 		Romain minus = new Romain("Minus", 6);
+
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 
 	}
 }
